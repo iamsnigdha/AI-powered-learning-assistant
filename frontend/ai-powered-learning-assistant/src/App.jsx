8132f6f1819615d8ad8a9e38d80755faa3a869f4
iamsnigdha <rsnigdha943@gmail.com>
2026-06-12 02:47:57 +0530

    Create authentication pages and auth context

--- frontend/ai-powered-learning-assistant/src/App.jsx
+++ frontend/ai-powered-learning-assistant/src/App.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import LoginPage from './pages/Auth/LoginPage';
+import RegisterPage from './pages/Auth/RegisterPage';
+import NotFoundPage from './pages/NotFoundPage';
 
-const App = () => {
+const App =() => {
+    const isAuthenticated = true
+    const loading = false
+
+    if (loading) {
+      return (
+        <div className="flex items-center justify-center h-screen">
+          <p>Loading...</p>
+      </div>
+    );
+  }
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={isAuthenticated ? <Navigate to=" /dashboard" replace /> : <Navigate to="/login" replace />}
+        />
+        <Route path= "/login" element= {<LoginPage />} />
+        <Route path= "/register" element= {<RegisterPage />} />
 
+        {/* Protected Routes */}
+        <Route element= {<ProtectedRoute />}>
+        <Route path="/dashboard " element = {<DashboardPage />} />
+        <Route path="/documents " element = {<DocumentListPage />} />
+        <Route path="/documents/:id" element = {<DocumentDetailPage />} />
+        <Route path="/flashcards " element = {<FlashCardsListPage />} />
+        <Route path="/documents/:id/flashcards" element = {<FlashcardPage />} />
+        <Route path="/quizzess/:quizId" element = {<QuizTakePage />} />
+        <Route path="/quizzess/:quizId/results" element = {<QuizResultPage />} />
+        <Route path="/profile" element = {<ProfilePage />} />
+      </Route>
+
+        <Route path="*" element= {<NotFoundPage />} />
+      </Routes>
+    </Router>
+  );
+}
 export default App
---
+++ frontend/ai-powered-learning-assistant/src/pages/Auth/Flashcards/FlashcardPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FlashcardPage = () => {
+  return (
+    <div>
+      FlashcardPage
+    </div>
+  )
+}
+
+export default FlashcardPage
