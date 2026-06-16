2b0b68ad1f2f928c56e68975fa1d0335c57efdeb
iamsnigdha <rsnigdha943@gmail.com>
2026-06-17 01:00:09 +0530

    chore: initialize backend project structure

---
+++ backend/middleware/errorHandler.js
@@ -0,0 +1,42 @@
+import { TokenExpiredError } from "jsonwebtoken";
+
+const errorHandler = (err, reg, res, next) => {
+    let statusCode = err.errstatusCode || 500;
+    let message = err.message || 'Server Error';
+
+    //Mongoose bad ObjectId
+    if (err.name === "CastError") {
+        message = 'Resource not found';
+        statusCode =404;
+
+    }
+    //Mongoose duplicate key
+    if (err.code === 11000) {
+        const field = Object.keys(err.keyValue)[0];
+        message = `${field} already exists`;
+        statusCode = 400;
+        
+    }
+    // Mongoose validation error
+    if (err.name === "ValidationError") {
+        message = 'File size exceeds the maximum limit of 10MB'
+        statusCode = 400;
+    }
+    // JWT errors
+    if (err.name === 'TokenExpiredError') {
+        message = 'Token expired'
+        statusCode = 401; 
+    }
+
+    console.error('Error:', {
+        message: err.message,
+        stack: process.env.NODE_ENV === 'development' ? err.stack : undefined
+    })
+    res.status(statusCode).json({
+        success: false,
+        error:message,
+        statusCode,
+        ...(process.env.NODE_ENV == 'development' && {stack: err.stack})
+
+    });
+};
